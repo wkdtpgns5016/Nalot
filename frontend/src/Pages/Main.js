f311d62512c3e44b45b399706cebbbbc20080cff
P1astic <53178556+P1astic@users.noreply.github.com>
2021-05-15 22:53:15 +0900

    #2 로그인 페이지 추가 (#8)
    * Nalot-2 로그인 페이지 추가 Test
    
    * Feat: 로그인 페이지 추가
    
    로그인 페이지 추가
    
    resolve#2

---
+++ frontend/src/Pages/Main.js
@@ -0,0 +1,13 @@
+import React from "react"
+import {withRouter} from 'react-router-dom'
+
+class Main{
+    render() {
+        return (
+            <div>
+                Main
+            </div>
+        )
+    }
+}
+export default withRouter(Main);
---
+++ frontend/src/Pages/index.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import {Route} from "react-router-dom"
+import Signin from './Signin'
+import Signup  from "./Signup";
+//import Main from "./Main"
+
+function Nalot({match}){
+    return(
+        <div>
+            <Route exact path = {match.path} component = {Signin}/>
+            <Route path = {`${match.path}/Signup`} component={Signup}/>
+        </div>
+    )
+}
+
+export default Nalot;
