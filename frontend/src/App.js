9981710d2505a45071dec73cbd873f36a29e9acd
P1astic <recon333527@gmail.com>
2021-05-15 22:43:10 +0900

    Feat: 로그인 페이지 추가
    로그인 페이지 추가
    
    resolve#2

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,16 @@
-import React, {useState, useEffect} from 'react';
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Nalot from './Pages'
+import Signup from './Pages/Signup'
+import {Route, BrowserRouter as Router} from 'react-router-dom'
 function App () {
-  const [message, setMessage] = useState("");
-  useEffect(() => {
-    fetch('/api/hello')
-        .then(response => response.text())
-        .then(message => {
-          setMessage(message);
-        });
-  },[])
   return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo"/>
-          <h1 className="App-title">{message}</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+          <Router>
+            <Route exact path ="/Nalot" component = {Nalot}/>
+            <Route path = "/Nalot/Signup" component = {Signup}/>
+          </Router>
       </div>
   )
 }
 export default App;
-//asdfasdf
