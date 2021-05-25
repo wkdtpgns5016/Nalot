e4bb09f88a9ce466a50964964766b513c052deb5
wkdtpgns5016 <74850752+wkdtpgns5016@users.noreply.github.com>
2021-05-25 17:29:40 +0900

    Fix: user_imformation 데이터 구조 변경 및 로직 수정 (#22)
    * Fix: userDto 속성 추가
    
    생년월일, 지번주소 속성 추가
    
    * Fix: Mapper 변경
    
    mybatis mapper 에서 update, insert 변경
    
    resolve #21

--- src/main/java/com/example/nalot/model/UserDto.java
+++ src/main/java/com/example/nalot/model/UserDto.java
@@ -3,6 +3,8 @@ package com.example.nalot.model;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.sql.Timestamp;
+
 @Setter
 @Getter
 public class UserDto {
@@ -13,4 +15,6 @@ public class UserDto {
     String zoneCode;
     String addressBasic;
     String addressDetail;
+    String addressGroundNumber;
+    Timestamp birth;
 }
