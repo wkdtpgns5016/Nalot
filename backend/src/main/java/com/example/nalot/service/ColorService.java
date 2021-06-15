b187312301c4d9163fb590ff0376b80898d0052f
wkdtpgns5016 <wkdtpgns5016@naver.com>
2021-06-15 17:25:06 +0800

    Feat: 톤인톤 배합 기능 추가
    1. rgb, hsl 객체 작성
    2. rgb-hsl 변환 기능 추가
    3. 톤인톤 배합 기능 추가
    
    resolve #51

---
+++ backend/src/main/java/com/example/nalot/service/ColorService.java
@@ -0,0 +1,8 @@
+package com.example.nalot.service;
+
+import com.example.nalot.model.RgbCode;
+import java.util.List;
+
+public interface ColorService {
+    public List<RgbCode> getToneInToneList(String hexCode);
+}
