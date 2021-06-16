265afdd42801602a799e2d9283312d0eaa410a48
wkdtpgns5016 <74850752+wkdtpgns5016@users.noreply.github.com>
2021-06-16 19:08:45 +0800

    Feat: 기온별 의상 추천 기능 추가 (#67)
    1. 기온별 의상 추천 기능 추가
    
    resolve #58

--- backend/src/main/java/com/example/nalot/service/ClothesService.java
+++ backend/src/main/java/com/example/nalot/service/ClothesService.java
@@ -1,8 +1,10 @@
 package com.example.nalot.service;
 
 import com.example.nalot.model.ClothesDto;
+import com.example.nalot.model.WeatherDto;
 import java.util.List;
 
 public interface ClothesService {
     public List<ClothesDto> selectWeatherListByCategory(String category);
+    public String sortCategoryByTemperature(WeatherDto weatherDto);
 }
