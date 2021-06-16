f3b763912780c2b649111abe25ffaf2d3bdc8913
P1astic <53178556+P1astic@users.noreply.github.com>
2021-06-17 02:50:21 +0900

    Feat: 메인 페이지 프레임 구성 (#69)
    메인 페이지 프레임, 사이드바 구성
    
    resolve #57

--- frontend/src/Pages/index.js
+++ frontend/src/Pages/index.js
@@ -2,13 +2,14 @@ import React from 'react'
 import {Route} from "react-router-dom"
 import Signin from './Signin'
 import Signup  from "./Signup";
-//import Main from "./Main"
+import Main from "./Main"
 
 function Nalot({match}){
     return(
         <div>
             <Route exact path = {`${match.path}`} component = {Signin}/>
-            <Route path = {`${match.path}/Signup`} component={Signup}/>
+            <Route path = {`${match.path}/signup`} component={Signup}/>
+            <Route path = {`${match.path}/main`} component={Main}/>
         </div>
     )
 }
