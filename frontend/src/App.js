3a4743be5818c696670a10455ac970714568f6f2
P1astic <53178556+P1astic@users.noreply.github.com>
2021-06-17 23:38:12 +0900

    Feat: 메인페이지 사이드바 버튼 기능 추가 (#78)
    사이드바 버튼 기능 추가
    
    resolve #70

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Nalot from './Pages'
 import Signup from './Pages/Signup'
 import Main from './Pages/Main'
+import myInformation from './Pages/myInformation'
 import {Route, BrowserRouter as Router} from 'react-router-dom'
 function App () {
   return (
@@ -10,7 +11,10 @@ function App () {
           <Router>
             <Route exact path ="/nalot" component = {Nalot}/>
             <Route path = "/nalot/signup" component = {Signup}/>
-              <Route path = "/nalot/main" component = {Main}/>
+            <Route path = "/nalot/main" component = {Main}/>
+            <Route path = "/main/myinfo" component = {myInformation}/>
+
+
           </Router>
       </div>
   )
