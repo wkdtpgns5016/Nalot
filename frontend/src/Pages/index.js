eb5585af78ff11808517442da35b8a5855d63fd9
P1astic <53178556+P1astic@users.noreply.github.com>
2021-09-11 16:18:49 +0900

    nalot-97 Feat: 의상 추천 페이지 구축 (#98)

--- frontend/src/Pages/index.js
+++ frontend/src/Pages/index.js
@@ -5,6 +5,8 @@ import Signup  from "./Signup";
 import Main from "./Main"
 import myInformation from"./myInformation"
 import recommendation from "./recommendation"
+import recommendation2 from "./recommendation2";
+import recommendation3 from "./recommendation3";
 
 function Nalot({match}){
     return(
@@ -13,7 +15,9 @@ function Nalot({match}){
             <Route path = {`${match.path}/signup`} component={Signup}/>
             <Route path = {`${match.path}/main`} component={Main}/>
             <Route path = {`${match.path}/myinformation`} component = {myInformation}/>
-            <Route path = {`${match.path}/recommendation`} compoenet = {recommendation}/>
+            <Route path = {`${match.path}/recommendation`} compoenent = {recommendation}/>
+            <Route path = {`${match.path}/recommendation2`} component = {recommendation2}/>
+            <Route path = {`${match.path}/recommendation3`} component = {recommendation3}/>
 
         </div>
     )
