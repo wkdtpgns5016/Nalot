f0b305ec6e68867571db84216974f90c80233dcb
hong <recon333527@gmail.com>
2021-09-14 00:39:45 +0900

    nalot-113 Feat: 의상 추천 페이지 구축

--- frontend/src/Pages/recommendation2.js
+++ frontend/src/Pages/recommendation2.js
@@ -1,13 +1,24 @@
 import React from 'react'
 import { useHistory, useLocation } from "react-router-dom";
-
 import Header from "../Components/Header";
 import Menu from "../Components/Menu";
+import reactCSS from 'reactcss'
+import {SketchPicker} from 'react-color'
+import axios from 'axios'
 
 function recommendation2(){
     const history = useHistory();
     const location = useLocation();
-
+    const [displayColorPicker, setdisplayColorPicker] = React.useState(false);
+    const [color, setcolor] = React.useState(
+        {
+            r : '241',
+            g : '112',
+            b : '19',
+            a : '1'
+        }
+    )
+    const [hex, sethex] = React.useState('#f17013')
 
     const prevClicked = () =>{
         history.push('/nalot/recommendation',{
@@ -28,39 +39,86 @@ function recommendation2(){
 
         })
     }
+
+    const openClicked = () =>{
+        setdisplayColorPicker(true)
+    }
+
+    const closeClicked = () =>{
+        setdisplayColorPicker(false)
+        console.log(hex)
+        axios.post('http://localhost:8080/colors/toneOnToneMixes',{
+            "hexCode" : hex
+        },{
+            headers:{
+                'Content-Type':'application/json',
+                'Authorization':`${location.state.key}`
+            }
+        }).then(response=>{
+            console.log(response.data)
+            axios.post('http://localhost:8080/colors/toneInToneMixes',{
+                "hexCode":hex
+            },{
+                headers:{
+                    'Content-Type':'application/json',
+                    'Authorization':`${location.state.key}`
+                }
+            }).then(res=>{
+                console.log(res.data)
+            })
+        })
+    }
+
+    const handleChange = (color) =>{
+        setcolor(color.rgb)
+        //console.log(color.hex)
+        sethex(color.hex)
+    }
+
+
+    const styles = reactCSS({
+        'default': {
+            color: {
+                width: '36px',
+                height: '14px',
+                borderRadius: '2px',
+                background: `rgba(${ color.r }, ${ color.g }, ${ color.b }, ${ color.a })`,
+            },
+            swatch: {
+                padding: '5px',
+                background: '#fff',
+                borderRadius: '1px',
+                boxShadow: '0 0 0 1px rgba(0,0,0,.1)',
+                display: 'inline-block',
+                cursor: 'pointer',
+            },
+            popover: {
+                position: 'absolute',
+                zIndex: '2',
+            },
+            cover: {
+                position: 'fixed',
+                top: '0px',
+                right: '0px',
+                bottom: '0px',
+                left: '0px',
+            },
+        },
+    });
     return(
+
         <div>
             <Header/>
             <Menu/>
             <div>
-                <span>선택한 의상</span>
-                &nbsp;
-                &nbsp;
-                &nbsp;
-                <span>톤온톤 매치</span>
-            </div>
-            <br></br>
-            <div>
-                <button>선택한 의상</button>
-                &nbsp;
-                &nbsp;
-                &nbsp;
-                <button>톤온톤 매치</button>
-            </div>
-            <br></br>
-            <div>
-                <span>선택한 색상</span>
-                &nbsp;
-                &nbsp;
-                &nbsp;
-                <span>톤인톤 매치</span>
-            </div>
-            <div>
-                <button>선택한 색상</button>
-                &nbsp;
-                &nbsp;
-                &nbsp;
-                <button>톤인톤 매치</button>
+                <div style={ styles.swatch } onClick={openClicked}>
+                    <div style={ styles.color } />
+                </div>
+                { displayColorPicker ? <div style={ styles.popover }>
+                    <div style={ styles.cover } onClick={closeClicked}/>
+                    <SketchPicker color={ color } onChange={ handleChange } />
+                </div> : null }
+
             </div>
             <div>
                 <button
