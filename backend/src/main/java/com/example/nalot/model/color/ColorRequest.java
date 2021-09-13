85853367df9f19d047252643f3cabba6a0e3eed1
wkdtpgns5016 <wkdtpgns5016@naver.com>
2021-09-14 00:34:35 +0900

    Feat: 색상 컨트롤러 추가
    색상 컨트롤러 추가
    
    resolve #114

---
+++ backend/src/main/java/com/example/nalot/model/color/ColorRequest.java
@@ -0,0 +1,14 @@
+package com.example.nalot.model.color;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ColorRequest {
+    String hexCode;
+}
