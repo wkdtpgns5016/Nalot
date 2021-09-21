4f64777ff8a50ab5ddfb8aac64ae0a060b22bbbd
hong <recon333527@gmail.com>
2021-09-21 18:36:28 +0900

    Feat: 내 정보 페이지 수정 탈퇴 기능 구축

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,6 +10,7 @@ import recommendation3 from "./Pages/recommendation3";
 import recommendation_location from "./Pages/recommendation_location";
 import myRecord from "./Pages/myRecord";
 import myRecord_detail from "./Pages/myRecord_detail";
+import myinformation_fix from "./Pages/myinformation_fix";
 import {Route, BrowserRouter as Router} from 'react-router-dom'
 function App () {
   return (
@@ -25,6 +26,7 @@ function App () {
             <Route path = "/nalot/recommendation_location" component = {recommendation_location}/>
             <Route path ="/nalot/myrecord" component = {myRecord}/>
             <Route path = "/nalot/myrecord_detail" component = {myRecord_detail}/>
+            <Route path ="/nalot/myinformation_fix" component = {myinformation_fix}/>
 
           </Router>
       </div>
--- frontend/src/Pages/myInformation.js
+++ frontend/src/Pages/myInformation.js
@@ -1,12 +1,42 @@
 import React from 'react';
 import Header from "../Components/Header"
 import Menu from "../Components/Menu"
+import axios from 'axios'
 
 
-import {useLocation} from "react-router-dom";
+import {useLocation, useHistory} from "react-router-dom";
 
 function myInformation(){
     const location = useLocation()
+    const history = useHistory()
+
+    const fixClicked=()=>{
+        history.push('/nalot/myinformation_fix',{
+            "key" : location.state.key,
+            "name" : location.state.name,
+            "id" : location.state.email,
+            "gender" : location.state.gender,
+            "zone_code" : location.state.zone_code,
+            "address_basic" : location.state.address_basic,
+            "address_detail" : location.state.address_detail,
+            "address_ground_number": location.state.address_ground_number,
+            "birth" : location.state.birth,
+            "email" : location.state.email,
+        })
+    }
+    const deleteClicked=()=>{
+        axios.delete('http://localhost:8080/users/'+location.state.email)
+            .then(res=>{
+                if(res.status===201){
+                    alert(res.data.message)
+                    history.push('/nalot')
+                }
+                }
+            ).catch(error=>{
+                alert(error.response.data.errorMessage)
+        })
+    }
+
     return(
         <div>
             <Header/>
@@ -34,6 +64,18 @@ function myInformation(){
                 <div>
                     birth {location.state.birth}
                 </div>
+            </div>
+            <div>
+                <button
+                    onClick={fixClicked}
+                >
+                    정보 수정
+                </button>
+                <button
+                    onClick={deleteClicked}
+                >
+                    회원 탈퇴
+                </button>
             </div>
 
         </div>
