1f4c9ff92188b62d41846e3d4ffdfa5ad946f79d
hong <recon333527@gmail.com>
2021-09-21 22:58:09 +0900

    Feat: 내 정보 페이지 디자인 수정

--- frontend/src/Pages/myInformation.js
+++ frontend/src/Pages/myInformation.js
@@ -5,8 +5,38 @@ import axios from 'axios'
 
 
 import {useLocation, useHistory} from "react-router-dom";
+import {makeStyles} from "@material-ui/core/styles";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import Avatar from "@material-ui/core/Avatar";
+import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
+import Typography from "@material-ui/core/Typography";
+import Grid from "@material-ui/core/Grid";
+import TextField from "@material-ui/core/TextField";
+import Button from "@material-ui/core/Button";
+import Container from "@material-ui/core/Container";
+
+const useStyles = makeStyles((theme) => ({
+    paper: {
+        marginTop: theme.spacing(8),
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+    },
+    avatar: {
+        margin: theme.spacing(1),
+        backgroundColor: theme.palette.secondary.main,
+    },
+    form: {
+        width: '100%', // Fix IE 11 issue.
+        marginTop: theme.spacing(3),
+    },
+    submit: {
+        margin: theme.spacing(3, 0, 2),
+    },
+}));
 
 function myInformation(){
+    const classes = useStyles()
     const location = useLocation()
     const history = useHistory()
 
@@ -39,45 +69,141 @@ function myInformation(){
 
     return(
         <div>
-            <Header/>
-            <Menu/>
-            <div>
-                이름 {location.state.name}
-                <div>
-                    이메일 {location.state.id}
-                </div>
-                <div>
-                    성별 {location.state.gender}
-                </div>
-                <div>
-                    zone_code {location.state.zone_code}
-                </div>
-                <div>
-                    address_basic {location.state.address_basic}
-                </div>
-                <div>
-                    address_detail {location.state.address_detail}
-                </div>
-                <div>
-                    address_ground_number {location.state.address_ground_number}
-                </div>
-                <div>
-                    birth {location.state.birth}
-                </div>
-            </div>
-            <div>
-                <button
-                    onClick={fixClicked}
-                >
-                    정보 수정
-                </button>
-                <button
-                    onClick={deleteClicked}
-                >
-                    회원 탈퇴
-                </button>
-            </div>
+        <Header/>
+        <Menu/>
+        <Container component="main" maxWidth="xs">
+            <CssBaseline />
+            <div className={classes.paper}>
+                <Avatar className={classes.avatar}>
+                    <LockOutlinedIcon />
+                </Avatar>
+                <Typography component="h1" variant="h5">
+                    회원 정보
+                </Typography>
+                <form className={classes.form} noValidate>
+                    <Grid container spacing={2}>
+                        <Grid item xs={12} sm={6}>
+                            <TextField
+                                name="firstName"
+                                variant="outlined"
+                                required
+                                fullWidth
+                                id="fname"
+                                label="이름"
+                                value={location.state.name}
+                                autoFocus
+                                inputProps={{maxLength:10}}
+                            />
+                        </Grid>
+                        <Grid item xs={12} sm ={6}>
+                            <TextField
+                                id="date"
+                                required
+                                variant={"outlined"}
+                                label="생년월일"
+                                type="date"
+                                defaultValue="2017-05-24"
+                                InputLabelProps={{
+                                    shrink: true,
+                                }}
+                            />
+                        </Grid>
+                        <Grid item xs={12} sm={6}>
+                            <TextField
+                                name="firstName"
+                                variant="outlined"
+                                required
+                                fullWidth
+                                id="gender"
+                                label="성별"
+                                value={location.state.gender}
+                                autoFocus
+                                inputProps={{maxLength:10}}
+                            />
+                        </Grid>
+                        <Grid item xs={12} sm = {8}>
+                            <TextField
+                                variant="outlined"
+                                required
+                                fullWidth
+                                id="postcode"
+                                label="우편번호"
+                                value={location.state.zone_code}
+                                name="postcode"
+                                inputProps={{maxLength: 5}}
+                            />
+                        </Grid>
+                        <Grid item xs={12}>
+                            <TextField
+                                variant="outlined"
+                                required
+                                fullWidth
+                                id="address1"
+                                label="기본주소"
+                                value={location.state.address_basic}
+                                name="address1"
+                                inputProps={{maxLength: 49}}
+                            />
+                        </Grid>
+                        <Grid item xs = {12}>
+                            <TextField
+                                variant="outlined"
+                                required
+                                fullWidth
+                                id="address2"
+                                label="지번주소"
+                                value={location.state.address_ground_number}
+                                name="address2"
+                                inputProps={{maxLength: 49}}
+                            />
+                        </Grid>
+                        <Grid item xs={12}>
+                            <TextField
+                                variant="outlined"
+                                required
+                                fullWidth
+                                id="address3"
+                                label="상세주소"
+                                value={location.state.address_detail}
+                                name="address3"
+                                inputProps={{maxLength: 49}}
+                            />
+                        </Grid>
 
+                        <Grid item xs={12}>
+                            <TextField
+                                variant="outlined"
+                                required
+                                fullWidth
+                                id="email"
+                                label="이메일 주소"
+                                value={location.state.id}
+                                name="email"
+                                inputProps={{maxLength: 44}}
+                            />
+                        </Grid>
+                    </Grid>
+                    <Button
+                        fullWidth
+                        variant="contained"
+                        color="primary"
+                        className={classes.submit}
+                        onClick={fixClicked}
+                    >
+                        정보수정
+                    </Button>
+                    <Button
+                        fullWidth
+                        variant="contained"
+                        color="primary"
+                        className={classes.submit}
+                        onClick={deleteClicked}
+                    >
+                        회원탈퇴
+                    </Button>
+                </form>
+            </div>
+        </Container>
         </div>
     )
 }
