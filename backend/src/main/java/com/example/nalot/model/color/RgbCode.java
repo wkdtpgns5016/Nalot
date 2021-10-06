a65b722eb8d627eb583f0eb96a5fb1a980cf416d
wkdtpgns5016 <74850752+wkdtpgns5016@users.noreply.github.com>
2021-10-06 22:27:54 +0900

    Feat: Rgb to Hsl (#231)
    Rgb to Hsl 기능 ColorService로 변환
    
    resolve #230

--- backend/src/main/java/com/example/nalot/model/color/RgbCode.java
+++ backend/src/main/java/com/example/nalot/model/color/RgbCode.java
@@ -20,39 +20,6 @@ public class RgbCode {
         this.blue = Integer.parseInt(hexCode.substring(5),16);
     }
 
-    public HslCode convertHsl(){
-        double redDelta = (double)this.red / 255;
-        double greenDelta = (double)this.green / 255;
-        double blueDelta = (double)this.blue / 255;
-
-        double cMax = Math.max(Math.max(redDelta, greenDelta), blueDelta);
-        double cMin = Math.min(Math.min(redDelta, greenDelta), blueDelta);
-        double delta = cMax - cMin;
-
-        int hue;
-        if(delta == 0) hue = 0;
-        else if(cMax == redDelta) {
-            double num = (greenDelta-blueDelta)/delta%6+6;
-            if(num < 0) num += 6;
-            hue = (int)(60 * num);
-        }
-        else if(cMax == greenDelta) {
-            hue = (int)(60 * (((blueDelta-redDelta)/delta)+2));
-        }
-        else {
-            hue = (int)(60 * (((redDelta-greenDelta)/delta)+4));
-        }
-        hue = hue % 360;
-
-        double lightness = (cMax+cMin) / 2;
-
-        double saturation;
-        if(delta == 0) saturation = 0;
-        else saturation = delta / (1-Math.abs(2*lightness-1));
-
-        return new HslCode(hue,saturation,lightness);
-    }
-
     public String toHexCode() {
         return "#" +
                 Integer.toHexString(this.red) +
