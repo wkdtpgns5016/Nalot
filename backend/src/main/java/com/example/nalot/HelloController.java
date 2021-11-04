6dd37e2cf0ea777fcbe60a4d8d7f7f54ce871863
wkdtpgns5016 <wkdtpgns5016@naver.com>
2021-11-04 14:36:44 +0900

    Docs: 스파크 개발 환경 구현
    스파크 개발 환경 구현
    
    resolve #251

--- backend/src/main/java/com/example/nalot/HelloController.java
+++ backend/src/main/java/com/example/nalot/HelloController.java
@@ -1,12 +1,32 @@
 package com.example.nalot;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Arrays;
 import java.util.Date;
+import java.util.List;
+import java.util.Map;
+
 @RestController
 public class HelloController {
+    @Autowired
+    JavaSparkContext sc;
+
     @GetMapping("/api/hello")
     public String hello(){
         return "안녕하세요. 현재 서버시간은 "+new Date() +"입니다. \n";
+    }
+
+    @RequestMapping(method = RequestMethod.POST, path = "/wordcount")
+    public Map<String, Long> count(@RequestBody String words) {
+        System.out.println("단어:" + words);
+        List<String> wordList = Arrays.asList(words.split("\\|"));
+
+        JavaRDD<String> words1 = sc.parallelize(wordList);
+        Map<String, Long> wordCounts = words1.countByValue();
+        return wordCounts;
     }
 }
