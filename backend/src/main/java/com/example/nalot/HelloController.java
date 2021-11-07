c4d2dcd5d4830e130196b0e58f8f348efb62889d
P1astic <53178556+P1astic@users.noreply.github.com>
2021-11-07 21:01:48 +0900

    Update HelloController.java

--- backend/src/main/java/com/example/nalot/HelloController.java
+++ backend/src/main/java/com/example/nalot/HelloController.java
@@ -14,21 +14,11 @@ import java.util.Map;
 
 @RestController
 public class HelloController {
-    @Autowired
-    JavaSparkContext sc;
-
+   
     @GetMapping("/api/hello")
     public String hello(){
         return "안녕하세요. 현재 서버시간은 "+new Date() +"입니다. \n";
     }
 
-    @RequestMapping(method = RequestMethod.POST, path = "/wordcount")
-    public Map<String, Long> count(@RequestBody String words) {
-        System.out.println("단어:" + words);
-        List<String> wordList = Arrays.asList(words.split("\\|"));
-
-        JavaRDD<String> words1 = sc.parallelize(wordList);
-        Map<String, Long> wordCounts = words1.countByValue();
-        return wordCounts;
-    }
+    
 }
